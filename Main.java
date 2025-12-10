4eba54008eedf4cd79a75565edb7f3fad25b71df
Mobinaalivandi <mobinavafa2006@gmail.com>
2025-12-10 03:02:43 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,35 @@
 import java.util.Scanner;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Owner's name :");
-        String ownerName = input.nextLine();
-        System.out.println("Initial balance");
-        double balance = input.nextDouble();
-        System.out.println("Accountsnumber :");
-        input.nextLine();
-        String accountNumber = input.nextLine();
-        BankAccount s = new BankAccount(accountNumber, ownerName, balance);
-        System.out.println("the amount is :");
+        Random ran = new Random();
         double amount = input.nextDouble();
-        s.deposit(amount);
-        s.withdraw(amount);
-        s.printBalance();
+        BankAccount su = new BankAccount();
+        int x = input.nextInt();
+        int[] id = new int[x];
+        BankAccount[] list = new BankAccount[x];
+        for (int j = 0; j < x; ++j) {
+            id[j] = ran.nextInt(899999) + 1000000;
+        }
+        for (int i = 0; i < x; ++i) {
+            String ownerName = input.next();
+            int accountNumber = ran.nextInt(899999) + 1000000;
+            for (int k = 0; k < i; ++k) {
+                if (accountNumber == id[k]) {
+                    accountNumber = ran.nextInt(899999) + 1000000;
+                }
+            }
+            id[i] = accountNumber;
+            BankAccount s = new BankAccount(accountNumber, ownerName, 0);
+            list[i] = s;
+            System.out.println(list[i].getownerName());
+            System.out.println(list[i].getaccountNumber());
+            System.out.println(list[i].getbalance());
+        }
+        su.deposit(amount);
+        su.withdraw(amount);
+        su.printBalance();
     }
 }
+
