05a65c42d5e3c8fba082f8ea30030940c8ec75e7
Mobinaalivandi <mobinavafa2006@gmail.com>
2025-12-10 03:05:08 -0800

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,51 +1,59 @@
 public class BankAccount {
-  private String accountNumber;
-  private  String ownerName;
-  private double balance;
-    BankAccount(String accountNumber, String ownerName, double balance) {
+    private int accountNumber;
+    private  String ownerName;
+    private int balance;
+    BankAccount(int accountNumber, String ownerName, int balance) {
         this.accountNumber = accountNumber;
         this.ownerName = ownerName;
-        this.balance = balance;
+        this.balance = 0 ;
+    }
+    public BankAccount() {
+
     }
     public String getownerName() {
         return ownerName;
     }
 
-    public String getaccountNumber() {
+    public int getaccountNumber() {
 
         return accountNumber;
     }
 
-    public double getbalance() {
+    public int getbalance() {
 
         return balance;
     }
 
     public void setownerName(String ownerName) {
+
         this.ownerName = ownerName;
     }
     double sum = 0;
     public void deposit(double amount) {
-        if ( amount > 0 ) {
-        sum = amount + balance;
-             System.out.println(" deposit amount is : " + sum);
+        if (amount > 0) {
+            sum = balance + amount;
+            System.out.println(" deposit amount is : " + sum);
         } else {
             System.out.println("this amount is not acceptable");
         }
     }
     double subtract = 0;
     public void withdraw(double amount) {
-        if (balance >= amount) {
+        if (balance >= amount && amount > 0) {
             subtract = balance - amount;
-                System.out.println("withdrawal amount is : " + subtract);
+            System.out.println("withdrawal amount is : " + subtract);
         } else {
             System.out.println("balance is not enough");
         }
     }
     public void printBalance() {
+
         System.out.println("current balance is : " + balance);
     }
 }
+
+
+
 
 
 
