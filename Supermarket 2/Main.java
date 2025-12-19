ca90467fcff500373689178876492d7ccab01d52
Mobinaalivandi <mobinavafa2006@gmail.com>
2025-12-19 03:32:31 -0800

    Update Main.java

--- Supermarket 2/Main.java
+++ Supermarket 2/Main.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Calculate cal = new Calculate();
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        Shop[] lists = new Shop[n];
+        for (int i = 0; i < n; ++i) {
+            double price = input.nextDouble();
+            int quantity = input.nextInt();
+            Shop list = new Shop(price, quantity);
+            String name = input.next();
+            list.name = name;
+            lists[i] = list;
+        }
+        cal.maximumprice(lists);
+        cal.minimumprice(lists);
+        cal.worth(lists);
+        cal.sort(lists);
+
+    }
+}
 
