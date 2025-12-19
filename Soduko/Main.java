741636c6d2ce4ef0e701de6d99f59cd815012c07
Mobinaalivandi <mobinavafa2006@gmail.com>
2025-12-19 10:25:46 -0800

    Update Main.java

--- Soduko/Main.java
+++ Soduko/Main.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
